1b75f42049a4621268f602aa1feebe373b415962
Yifat-t <78460287+Yifat-t@users.noreply.github.com>
2021-08-16 17:09:19 -0400

    Add files via upload

---
+++ MHC_Hospital_Redesign/Migrations/202108162029248_adding username required in feedback model.cs
@@ -0,0 +1,18 @@
+namespace MHC_Hospital_Redesign.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addingusernamerequiredinfeedbackmodel : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Feedbacks", "UserName", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Feedbacks", "UserName", c => c.String());
+        }
+    }
+}
